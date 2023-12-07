2c34420d9e477f0ecce55d05ad0175a71edfc1fb
naganaidu531 <153172477+naganaidu531@users.noreply.github.com>
2023-12-07 14:25:43 +0530

    Update streamlit_app.py
    added my mom's new healthy diner

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
-streamlit.title(' My Parents New Healthy Diner')
-streamlit.header(' Breakfast Menu')
-streamlit.text(' omega 3 & Blueberry oatmeal')
-streamlit.text('kale,spinach & Rocket smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.title(' My Mom'S New Healthy Diner')
+streamlit.header(' Breakfast Favorites')
+streamlit.text('  🥣 omega 3 & Blueberry oatmeal')
+streamlit.text('🥗 kale,spinach & Rocket smoothie')
+streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
