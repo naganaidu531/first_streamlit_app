6628bcb3ba8afbd1a898e3acbcfef4f1857799c6
naganaidu531 <153172477+naganaidu531@users.noreply.github.com>
2023-12-07 14:29:23 +0530

    Update streamlit_app.py
    added my mom breakfast

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
-streamlit.title(' My Mom'S New Healthy Diner')
-streamlit.header(' Breakfast Favorites')
-streamlit.text('  🥣 omega 3 & Blueberry oatmeal')
+streamlit.title('My Mom'S New Healthy Diner')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 omega 3 & Blueberry oatmeal')
 streamlit.text('🥗 kale,spinach & Rocket smoothie')
-streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
